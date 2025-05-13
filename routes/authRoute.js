58ca4186e700f59adb15578c0939bd2290e490ae
ashna <ashnakarim2024@mca.ajce.in>
2025-05-14 04:31:39 +0530

    commit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authMiddleware = require('../middleware/authMiddleware');
-const UserModel = require('../models/Usermodel'); 
+const UserModel = require('../models/UserModel'); 
 
 const router = express.Router();
 
